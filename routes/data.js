2cb6168ad7f790217351047a4ebe1cdb279a2f19
ricardovera76 <ricardovera71@gmail.com>
2022-08-29 14:22:38 -0400

    fix: fixed some issues

--- routes/data.js
+++ routes/data.js
@@ -4,22 +4,26 @@ const { readDB, readLogs, VALUES } = require("../helpers/sheets");
 
 router.post("/", async (req, res) => {
   const { value, type } = req.body;
-  const searchType = VALUES[type];
-  const result = await readDB(value, searchType);
-  const data = {
-    NIF: result[2],
-    NOMBRE: result[3],
-    TELEFONO: result[4],
-    DIRECCION: `${result[5]} ${result[6]} ${result[7]}`,
-    OLT: `OLTx${result[8]}`,
-    PUERTO_OLT: `${result[9]}/${result[10]}/${result[11]}/${result[12]}`,
-    NOMENCLATURA: result[13],
-    EQUIPO: result[14],
-    MAC: result[15],
-    SERIAL: result[16],
-    PROVEEDOR: result[17],
-  };
-  res.send(data);
+  try {
+    const searchType = VALUES[type];
+    const result = await readDB(value, searchType);
+    const data = {
+      NIF: result[2],
+      NOMBRE: result[3],
+      TELEFONO: result[4],
+      DIRECCION: `${result[5]} ${result[6]} ${result[7]}`,
+      OLT: `OLTx${result[8]}`,
+      PUERTO_OLT: `${result[9]}/${result[10]}/${result[11]}/${result[12]}`,
+      NOMENCLATURA: result[13],
+      EQUIPO: result[14],
+      MAC: result[15],
+      SERIAL: result[16],
+      PROVEEDOR: result[17],
+    };
+    res.send(data);
+  } catch (err) {
+    res.send(err);
+  }
 });
 
 router.get("/", async (req, res) => {
